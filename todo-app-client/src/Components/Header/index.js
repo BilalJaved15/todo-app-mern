7c17d66c6e43680aa5852fa56fbc8e41f72986c3
Syed Faateh Sultan Kazmi <54814772+faatehsultan@users.noreply.github.com>
2021-10-04 14:27:20 +0500

    improved the look and feel overall

--- todo-app-client/src/Components/Header/index.js
+++ todo-app-client/src/Components/Header/index.js
@@ -6,8 +6,8 @@ function Header(props) {
   return (
     <div>
       <div className="header">
-        <h1>🚀 TODO App 🚀 </h1>
-        <h3>Create a todo list for yourself, Simple and easy! 💯</h3>
+        <h1>TaskBook 📚 </h1>
+        <h2>Focus on productive stuff, cuz TaskBook is here to handle your schedules 😉</h2>
       </div>
     </div>
   );
