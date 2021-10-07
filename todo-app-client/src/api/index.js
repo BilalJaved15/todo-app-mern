ff1f739a671136a26971ebf92f78344b9ec124f1
Syed Faateh Sultan Kazmi <54814772+faatehsultan@users.noreply.github.com>
2021-10-07 15:39:42 +0500

    added extra layer of security by integrating additional api secret key check on every api call that modifies the database

--- todo-app-client/src/api/index.js
+++ todo-app-client/src/api/index.js
@@ -1,25 +1,29 @@
 import axios from "axios";
 
+const api_secret = "YBL4Mcr7aT7EPlOoCApAL2v3Tb3XrcvPUvDIycJ0Sz5oxZ82d7";
+
 export const GET_ITEM = () =>
-    axios({
-        method: "GET",
-        url: (process.env.REACT_APP_TODO_SERVER_URL + "/get-items"),
-    });
+  axios({
+    method: "GET",
+    url: process.env.REACT_APP_TODO_SERVER_URL + "/get-items",
+  });
 
 export const ADD_ITEM = (utitle) =>
-    axios({
-        method: "POST",
-        url: (process.env.REACT_APP_TODO_SERVER_URL + "/add-item"),
-        data: {
-            title: utitle
-        }
-    });
+  axios({
+    method: "POST",
+    url: process.env.REACT_APP_TODO_SERVER_URL + "/add-item",
+    data: {
+      api_secret: api_secret,
+      title: utitle,
+    },
+  });
 
 export const DELETE_ITEM = (uid) =>
-    axios({
-        method: "DELETE",
-        url: (process.env.REACT_APP_TODO_SERVER_URL + "/delete-item"),
-        data: {
-            id: uid
-        }
-    })
+  axios({
+    method: "DELETE",
+    url: process.env.REACT_APP_TODO_SERVER_URL + "/delete-item",
+    data: {
+      api_secret: api_secret,
+      id: uid,
+    },
+  });
